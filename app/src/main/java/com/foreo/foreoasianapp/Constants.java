f24cb31694b9b7734a03840b109c089e98407fe5
houjiguo <jiguo.hou@dragonsoftbravo.com>
2018-03-27 16:55:22 +0800

    微信支付

---
+++ app/src/main/java/com/foreo/foreoasianapp/Constants.java
@@ -0,0 +1,11 @@
+package com.foreo.foreoasianapp;
+
+public class Constants {
+    public static final String APP_ID = "wxe50753e56f908fb7";
+
+    public static class ShowMsgActivity {
+		public static final String STitle = "showmsg_title";
+		public static final String SMessage = "showmsg_message";
+		public static final String BAThumbData = "showmsg_thumb_data";
+	}
+}
